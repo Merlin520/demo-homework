3b7b2ee9162fe876411a4e918ed86dd2efafc06b
meiya <meiya90hou@163.com>
2017-12-27 00:47:58 +0800

    轮播思路

---
+++ carousel - 副本/main.js
@@ -0,0 +1,82 @@
+setTimeout(function () {
+    $('.image>img:nth-child(1)').css({
+        transform:'translateX(-100%)'
+    });
+
+    $('.image>img:nth-child(2)').css({
+        transform:'translateX(-100%)'
+    });
+    $('.image>img:nth-child(1)').one('transitionend',function (e) {
+       $(e.currentTarget).addClass('right').css({transform:'none'})
+    })
+
+},3000);
+
+setTimeout(function () {
+    $('.image>img:nth-child(2)').css({
+        transform:'translateX(-200%)'
+    });
+
+    $('.image>img:nth-child(3)').css({
+        transform:'translateX(-100%)'
+    });
+    $('.image>img:nth-child(2)').one('transitionend',function (e) {
+        $(e.currentTarget).addClass('right').css({transform:'none'})
+    })
+
+},6000);
+
+setTimeout(function () {
+    $('.image>img:nth-child(3)').css({
+        transform:'translateX(-200%)'
+    });
+
+    $('.image>img:nth-child(1)').css({
+        transform:'translateX(-100%)'
+    });
+    $('.image>img:nth-child(3)').one('transitionend',function (e) {
+        $(e.currentTarget).addClass('right').css({transform:'none'})
+    })
+
+},9000);
+
+setTimeout(function () {
+    $('.image>img:nth-child(1)').css({
+        transform:'translateX(-200%)'
+    });
+
+    $('.image>img:nth-child(2)').css({
+        transform:'translateX(-100%)'
+    });
+    $('.image>img:nth-child(1)').one('transitionend',function (e) {
+        $(e.currentTarget).addClass('right').css({transform:'none'})
+    })
+
+},12000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
